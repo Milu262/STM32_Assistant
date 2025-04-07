a4220a106cded1e102ef14e8fbadb4a895cc13fd
Ankesiqi <1261807915@qq.com>
2025-04-07 20:39:55 +0800

    将查找I2C总线上的设备按钮函数移到I2C_Component_Control文件里

--- Form1.cs
+++ Form1.cs
@@ -28,26 +28,6 @@ namespace STM32_Assistant
         }
 
 
-        private void find_i2c_device_button_Click(object sender, EventArgs e)
-        {
-            byte[] send_data = new byte[3];
-            send_data[0] = Frame_header;//固定帧头
-            if (!I2C_serialPort.IsOpen)//串口没有打开
-            {
-                MessageBox.Show("串口未打开", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                uart_I2C_control_button.Text = "打开串口";
-                return;
-            }
-            send_data[1] = 0x02;//8bit寄存器地址模式
-            send_data[2] = 0x05;//查找I2C总线上的设备
-            try
-            {
-                I2C_serialPort.Write(send_data, 0, 3);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+
     }
 }
